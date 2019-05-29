2985aa353e08bac88aa6e5825341bf7abc84639e
gaozhanghappy <gaozhanghappy@126.com>
2019-05-29 16:14:47 +0800

    refresh的手动刷新功能

--- config-client/src/main/java/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/springcloud/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope  //开启refresh机制
 public class ConfigClientApplication {
 	@Value("${clientParam}")
 	private String clientParam;
